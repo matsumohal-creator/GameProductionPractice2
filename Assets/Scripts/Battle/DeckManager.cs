bb738cc28978f70fb9fa7a6c009f9bd3e8aba8b5
fujita8610 <haruto.2004.5.6@gmail.com>
2026-06-04 15:13:36 +0900

    デッキマネージャーの作成

--- Assets/Scripts/Battle/DeckManager.cs
+++ Assets/Scripts/Battle/DeckManager.cs
@@ -3,5 +3,54 @@ using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
-  
+    //インスペクターで初期デッキを設定できるようにするためのフィールド
+    [Header("初期デッキ")]
+    [SerializeField]
+
+    // スキルデータのリストを初期デッキとして設定
+    private List<SkillData> startDeck = new();
+
+    // ドローとディスカードの管理のためのリスト
+    private List<SkillData> drawPile = new();
+
+    // ドローしたカードを置くためのリスト
+    private List<SkillData> discardPile = new();
+
+    // ドローパイルとディスカードパイルを外部からアクセスできるようにする
+    public List<SkillData> DrawPile => drawPile;
+
+    // ドローパイルとディスカードパイルを外部からアクセスできるようにする
+    public List<SkillData> DiscardPile => discardPile;
+
+    private void Awake()
+    {
+        // デッキを初期化する
+        InitializeDeck();
+    }
+
+    public void InitializeDeck()
+    {
+        // ドローパイルとディスカードパイルをクリアして
+        drawPile.Clear();
+        discardPile.Clear();
+
+        // 初期デッキを山札に追加
+        drawPile.AddRange(startDeck);
+
+        // 山札をシャッフルする
+        Shuffle();
+    }
+
+    // 山札をシャッフルするメソッド
+    public void Shuffle()
+    {
+        for (int i = 0; i < drawPile.Count; i++)
+        {
+            int randomIndex = Random.Range(i, drawPile.Count);
+
+            SkillData temp = drawPile[i];
+            drawPile[i] = drawPile[randomIndex];
+            drawPile[randomIndex] = temp;
+        }
+    }
 }

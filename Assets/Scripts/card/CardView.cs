8766955071b55549fc7be8ef4cc0fdc71fcc3224
matuyume6231-crypto <matuyume6231@gmail.com>
2026-06-09 20:16:51 +0900

    カードイラストを実装しました。

---
+++ Assets/Scripts/card/CardView.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// カードのUIを管理するクラス
+// カードのUIは、カードの名前、コスト、説明、アイコンなどを表示するためのクラスです。
+// カードのUIは、CardViewクラスとして実装されており、カードのデータを受け取ってUIを更新するためのSetup関数を持っています。
+
+public class CardView : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TMP_Text nameText;
+    [SerializeField] private TMP_Text costText;
+    [SerializeField] private TMP_Text explanationText;
+    [SerializeField] private Image cardImage;
+
+    private SkillData skillData;
+
+    public SkillData SkillData => skillData;
+
+    public void Setup(SkillData skill)
+    {
+        if (skill == null)
+        {
+            return;
+        }
+
+        skillData = skill;
+
+        nameText.text = skill.skillName;
+        costText.text = skill.cost.ToString();
+        explanationText.text = skill.description;
+        cardImage.sprite = skill.icon;
+    }
+}

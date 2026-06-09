6135df66d2071f504275beba81b73e2f0e7b3870
yoshimasa0610 <higuti.yoshimasa@gmail.com>
2026-06-09 15:13:58 +0900

    ok

---
+++ Assets/Scripts/Scene/EditScene.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditScene : MonoBehaviour
+{
+    private List<int> selectedFlg;
+
+    public void Character0Click()
+    {
+        
+    }
+    public void Character1Click()
+    {
+
+    }
+    public void Character2Click()
+    {
+
+    }
+    public void Character3Click()
+    {
+
+    }
+    public void Character4Click()
+    {
+
+    }
+    public void Character5Click()
+    {
+
+    }
+}

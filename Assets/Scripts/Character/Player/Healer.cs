ee50c308f9b764fc118e7a3ff48d6a5c5766710c
yoshimasa0610 <higuti.yoshimasa@gmail.com>
2026-07-14 11:47:22 +0900

    ぷれいやーつくった後頼む

---
+++ Assets/Scripts/Character/Player/Healer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Healer : PlayerBase
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Character/Player/Tank.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Tank : PlayerBase
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
